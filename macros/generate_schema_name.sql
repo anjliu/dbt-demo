00278aeacf8f21558ba0591359631d9d34ae8e22
Anjie Liu <anjieliu.al@gmail.com>
2025-04-16 20:50:54 +0000

    added schema unification for staging environment

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -2,6 +2,7 @@
 -- by concatenating the user's profile name and any customized schema names specified in the model
 -- while this is a good idea in dev environments, prod (and prod-like) environments would need to have a unified schema structure
 -- this script removes the default concatenation behaviour in prod environments
+-- the env_var function grabs the value of a user-set environment variable as the first arugment and takes an optional second argument as the default value
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
 
@@ -10,6 +11,10 @@
 
         {{ default_schema }}
 
+    {%- elif  env_var('DBT_ENV_TYPE','DEV') == 'PROD' -%}
+        
+        {{ custom_schema_name | trim }}
+    
     {%- elif  env_var('DBT_ENV_TYPE','DEV') == 'PROD' -%}
         
         {{ custom_schema_name | trim }}
